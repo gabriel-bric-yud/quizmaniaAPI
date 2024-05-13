2467ebf175dfc0505a8ffe831f03a416e109103d
gbabyblue <114004755+gbabyblue@users.noreply.github.com>
2024-05-13 03:13:19 -0400

    Update index.php
    update origin

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-  header('Access-Control-Allow-Origin: http://127.0.0.1:5502');
+  header('Access-Control-Allow-Origin: https://quizmania-online.azurewebsites.net');
   header('Access-Control-Allow-Methods: GET, POST');
   header("Access-Control-Allow-Headers: Content-Type");
 
